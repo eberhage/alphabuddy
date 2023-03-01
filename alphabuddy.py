74d8c5d8eac182b9660200986b5e78cecb11ea7a
Jan Eberhage <jan.eberhage+git@gmail.com>
2023-03-01 13:40:13 +0100

    code cleanup (untested)

--- alphabuddy.py
+++ alphabuddy.py
@@ -1,6 +1,9 @@
-__version_info__ = (1, 0, 3)
+__version_info__ = (1, 0, 4)
 __version__ = ".".join(map(str, __version_info__))
-__author__ = "Jan Eberhage, Institute for Biophysical Chemistry, Hannover Medical School (eberhage.jan@mh-hannover.de)"
+__author__ = (
+    "Jan Eberhage, Institute for Biophysical Chemistry, "
+    "Hannover Medical School (eberhage.jan@mh-hannover.de)"
+)
 
 import os
 import sys
@@ -10,8 +13,9 @@ import json
 import logging
 import time
 import datetime
-import pathlib
+from pathlib import Path
 import subprocess
+import importlib.util
 
 
 class CustomFormatter(logging.Formatter):
@@ -39,115 +43,140 @@ class CustomFormatter(logging.Formatter):
 class AlphaFoldJob:
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
-        self.fasta_paths = os.path.join(
-            self.output_dir, self.name, self.name + ".fasta"
-        )
+        self.alphafold_path = Path(self.alphafold_path)
+        self.data_dir = Path(self.data_dir)
+        self.output_dir = Path(self.output_dir)
+        self.job_dir = self.output_dir / self.name
+        self.fasta_paths = self.job_dir / f"{self.name}.fasta"
         log.info("Creating the following job:")
         for key, val in self.__dict__.items():
-            log.info(key.ljust(35, " ") + ": " + str(val))
+            log.info(f"{key.ljust(35)}: {val}")
 
     def generate_fasta(self):
-        pathlib.Path(os.path.join(self.output_dir, self.name)).mkdir(
-            parents=True, exist_ok=True
-        )
+        self.job_dir.mkdir(parents=True, exist_ok=True)
         with open(self.fasta_paths, "w") as f:
-            for key, val in self.sequences.items():
-                f.write(">" + key + "\n")
-                f.write(val + "\n")
+            [
+                f.write(f">{key}\n{val}\n")
+                for key, val
+                in self.sequences.items()
+            ]
 
     def run_alphafold(self):
         subprocess_list = ["python3"]
         subprocess_list.append(
-            os.path.join(
-                self.alphafold_path, "alphafold-main", "docker", "run_docker.py"
-            )
+            self.alphafold_path / "alphafold-main" / "docker" / "run_docker.py"
         )
-        subprocess_list.append("--max_template_date=" + str(self.max_template_date))
-        subprocess_list.append("--data_dir=" + self.data_dir)
-        subprocess_list.append("--docker_user=" + self.docker_user)
-        subprocess_list.append("--output_dir=" + self.output_dir)
-        subprocess_list.append("--fasta_paths=" + self.fasta_paths)
-        if hasattr(self, "model_preset"):
-            subprocess_list.append("--model_preset=" + self.model_preset)
-        if hasattr(self, "num_multimer_predictions_per_model"):
-            subprocess_list.append(
-                "--num_multimer_predictions_per_model="
-                + str(self.num_multimer_predictions_per_model)
-            )
-        if hasattr(self, "models_to_relax"):
-            subprocess_list.append("--models_to_relax=" + self.models_to_relax)
+
+        for param in [
+            "max_template_date",
+            "data_dir",
+            "docker_user",
+            "output_dir",
+            "fasta_paths",
+        ]:
+            subprocess_list.append(f"--{param}={getattr(self, param)}")
+
+        for optional_param in [
+            "model_preset",
+            "num_multimer_predictions_per_model",
+            "models_to_relax",
+        ]:
+            if hasattr(self, optional_param):
+                subprocess_list.append(
+                    f"--{optional_param}={getattr(self, optional_param)}"
+                )
 
         af_process = subprocess.run(subprocess_list)
         return af_process.returncode
 
     def print_job_details(self):
-        with open(
-            os.path.join(self.output_dir, self.name, "alphabuddy_job_details.json"), "w"
-        ) as f:
+        with open(self.job_dir / "alphabuddy_job_details.json", "w") as f:
             json.dump(self.__dict__, f, indent=2)
 
     def run_alphaplots(self, settings):
         if not hasattr(self, "alphaplots"):
             pass
-        alphaplots_path = settings["alphaplots"]["path"]
+        alphaplots_path = Path(settings["alphaplots"]["path"])
         subprocess_list = [
             "python3",
             alphaplots_path,
-            "--input_dir=" + os.path.join(self.output_dir, self.name),
+            f"--input_dir={self.job_dir}",
             "--yes",
         ]
-        if "rmpkl" in self.alphaplots:
-            subprocess_list.append("--rmpkl")
-        if "jsondump" in self.alphaplots:
-            subprocess_list.append("--jsondump")
+
+        for param in ["rmpkl", "jsondump"]:
+            if param in self.alphaplots:
+                subprocess_list.append(f"--{param}")
+
         subprocess.run(subprocess_list)
 
 
+def setup_logging():
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.DEBUG)
+    console_handler.setFormatter(CustomFormatter())
+    logger.addHandler(console_handler)
+    return logger
+
+
 def check_settings(settings):
-    if "versions" not in settings or not type(settings["versions"]) is dict:
+    if (
+        "versions" not in settings or
+        not isinstance(settings["versions"], dict)
+    ):
         log.error(
-            "The settings file seems to have a bad layout for the »versions«. It should be a dictionary. Exiting."
+            "The settings file seems to have a bad layout for the »versions«. "
+            "It should be a dictionary. Exiting."
         )
         sys.exit(1)
 
     default_version = next(
         (
             version
-            for version in settings["versions"].keys()
-            if "default" in settings["versions"][version].keys()
-            and settings["versions"][version]["default"] == True
+            for version, version_params in settings["versions"].items()
+            if version_params.get("default") is True
         ),
         None,
     )
 
     if not default_version:
         log.error(
-            "No default version was chosen under »versions«/<version> in the settings. Exiting."
+            "No default version was chosen under »versions«/<version> in the "
+            "settings. Exiting."
         )
         sys.exit(1)
-    if not "data_dir" in settings["versions"][default_version]:
+    if not settings["versions"][default_version].get("data_dir"):
         log.error(
-            "A »data_dir« has to be provided under »versions«/<version> in the settings. Exiting."
+            "A »data_dir« has to be provided under »versions«/<version> in "
+            "the settings. Exiting."
         )
         sys.exit(1)
-    if not "path" in settings["versions"][default_version]:
+    if not settings["versions"][default_version].get("path"):
         log.error(
-            "A »path« has to be provided under »versions«/<version> in the settings. Exiting."
+            "A »path« has to be provided under »versions«/<version> in the "
+            "settings. Exiting."
         )
         sys.exit(1)
 
-    if "alphaplots" in settings:
+    if settings.get("alphaplots"):
         try:
-            from matplotlib import pyplot as plt
-        except ModuleNotFoundError:
+            module_spec = importlib.util.find_spec("matplotlib")
+            if module_spec is None:
+                log.error("Module »matplotlib« is not installed.")
+                log.error("Please try »python3 -m pip install matplotlib«.")
+                sys.exit(1)
+        except ImportError:
             log.error("Module »matplotlib« is not installed.")
             log.error("Please try »python3 -m pip install matplotlib«.")
             sys.exit(1)
-        if not "path" in settings["alphaplots"] or not os.path.exists(
+        if not settings["alphaplots"].get("path") or not os.path.exists(
             settings["alphaplots"]["path"]
         ):
             log.error(
-                f"»alphaplots« was not found under the path given in the settings. Aborting."
+                "»alphaplots« was not found under the path given in the "
+                "settings. Aborting."
             )
             sys.exit(1)
 
@@ -158,36 +187,49 @@ def get_next_job(path):
     jobs = [
         entry
         for entry in os.scandir(path)
-        if entry.is_file()
-        and (entry.name.endswith(".yaml") or entry.name.endswith(".yml"))
+        if entry.is_file() and entry.name.endswith((".yaml", ".yml"))
     ]
-    jobs.sort(key=lambda x: os.path.getmtime(x))
+
     if not jobs:
         return False
+    jobs.sort(key=lambda x: os.path.getmtime(x))
     for job in jobs:
         job_dict = yaml.safe_load(open(job.path))
-        if "urgent" in job_dict.keys() and job_dict["urgent"]:
+        if "urgent" in job_dict and job_dict["urgent"] is True:
             return job
     return jobs[0]
 
 
 def check_config(job, settings):
-    job_dict = yaml.safe_load(open(job.path))
+    try:
+        with open(job.path, "r") as f:
+            job_dict = yaml.safe_load(f)
+    except Exception:
+        log.warning(f"The file »{job.path}« could not be loaded. Skipping.")
+        return False
 
-    if "version" in job_dict and job_dict["version"] not in settings["versions"].keys():
+    if (
+        "version" in job_dict and
+        job_dict["version"] not in settings["versions"]
+    ):
         log.warning(
-            f"The file »{job.path}« contains a value for the key »version« that is not included in your settings file. Skipping."
+            f"The file »{job.path}« contains a value for the key »version« "
+            "that is not included in your settings file. Skipping."
         )
         return False
 
-    if not "sequences" in job_dict.keys():
+    if "sequences" not in job_dict:
         log.warning(
-            f"The file »{job.path}« has no »sequences«. This is mandatory. Skipping."
+            f"The file »{job.path}« has no »sequences«. This is mandatory. "
+            "Skipping."
         )
         return False
-    if not type(job_dict["sequences"]) is dict:
+
+    sequences = job_dict["sequences"]
+    if not isinstance(sequences, dict) or not sequences:
         log.warning(
-            f"The file »{job.path}« seems to have a bad layout for the »sequences«. It should be an indented dictionary. Skipping."
+            f"The file »{job.path}« seems to have a bad layout for the "
+            "»sequences«. It should be an indented dictionary. Skipping."
         )
         return False
 
@@ -197,75 +239,63 @@ def check_config(job, settings):
 def create_alphafold_job(job, settings, args):
     job_dict = yaml.safe_load(open(job.path))
 
-    if not "version" in job_dict.keys():
-        job_dict["version"] = next(
+    job_dict.setdefault(
+        "version",
+        next(
             i
-            for i in settings["versions"].keys()
-            if settings["versions"][i]["default"] == True
-        )
+            for i, version in settings["versions"].items()
+            if version.get("default")
+        ),
+    )
+    job_dict.setdefault("name", Path(job.path).stem)
+    job_dict.setdefault(
+        "max_template_date", datetime.datetime.today().strftime("%Y-%m-%d")
+    )
+    job_dict.setdefault(
+        "output_dir",
+        settings.get("output_dir", os.path.join(args.directory, "results")),
+    )
 
-    job_dict["data_dir"] = settings["versions"][job_dict["version"]]["data_dir"]
-    job_dict["alphafold_path"] = settings["versions"][job_dict["version"]]["path"]
-
-    if not "name" in job_dict.keys():
-        job_dict["name"] = os.path.splitext(pathlib.Path(job.path).name)[0]
-
-    if "docker_user" in settings.keys():
-        job_dict["docker_user"] = settings["docker_user"]
-    else:
-        job_dict["docker_user"] = "root"
-
-    if not "output_dir" in job_dict.keys():
-        if "output_dir" in settings.keys():
-            job_dict["output_dir"] = settings["output_dir"]
-        else:
-            job_dict["output_dir"] = os.path.join(args.directory, "results")
-
-    if not "max_template_date" in job_dict.keys():
-        job_dict["max_template_date"] = datetime.datetime.today().strftime("%Y-%m-%d")
+    job_dict["data_dir"] = settings["versions"][job_dict["version"]].get(
+        "data_dir"
+    )
+    job_dict["alphafold_path"] = settings["versions"][job_dict["version"]].get(
+        "path"
+    )
+    job_dict["docker_user"] = settings.get("docker_user", "root")
 
     return AlphaFoldJob(**job_dict)
 
 
-def move_job_to_failed(job, directory):
-    failed_path = os.path.join(directory, "failed_jobs")
-    if not os.path.exists(failed_path):
-        os.mkdir(failed_path)
-    os.rename(job.path, os.path.join(failed_path, job.name))
-    log.info(f"Moving job to »{failed_path}«.")
-
-
-def move_job_to_done(job, directory):
-    done_path = os.path.join(directory, "done_jobs")
-    if not os.path.exists(done_path):
-        os.mkdir(done_path)
-    os.rename(job.path, os.path.join(done_path, job.name))
-    log.info(f"Moving job to »{done_path}«.")
+def move_job(job, directory, target_dir_name):
+    target_dir_path = os.path.join(directory, target_dir_name)
+    if not os.path.exists(target_dir_path):
+        os.mkdir(target_dir_path)
+    os.rename(job.path, os.path.join(target_dir_path, job.name))
+    log.info(f"Moving job to »{target_dir_path}«.")
 
 
 def main():
     global log
-    log = logging.getLogger(__name__)
-    log.setLevel(logging.DEBUG)
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.DEBUG)
-    ch.setFormatter(CustomFormatter())
-    log.addHandler(ch)
+    log = setup_logging()
 
     parser = argparse.ArgumentParser(
         add_help=False,
-        description="This script will run AlphaFold with the given sequences and configurations provided in the directory.",
+        description=(
+            "This script will run AlphaFold with the given sequences "
+            "and configurations provided in the directory."
+            )
     )
     parser.add_argument(
         "directory",
         metavar="<alphabuddy_dir>",
-        help="Relative or absolute path to the alphabuddy directory",
+        help="Relative or absolute path to the alphabuddy directory"
     )
     parser.add_argument(
         "-v",
         "--version",
         action="version",
-        version="%(prog)s (" + __version__ + ") " + " by " + __author__,
+        version="%(prog)s (" + __version__ + ") " + " by " + __author__
     )
     parser.add_argument(
         "-h", "--help", action="help", help="show this help message and exit"
@@ -281,7 +311,8 @@ def main():
         settings_path = os.path.join(args.directory, "settings.yaml")
         if not os.path.exists(settings_path):
             log.error(
-                f"The file »{settings_path}« is mandatory. Please provide the settings at this specific location."
+                f"The file »{settings_path}« is mandatory. Please provide the "
+                "settings at this specific location."
             )
             sys.exit(1)
 
@@ -296,20 +327,19 @@ def main():
                 job.generate_fasta()
                 code = job.run_alphafold()
                 if code:
-                    move_job_to_failed(next_job, args.directory)
+                    move_job(next_job, args.directory, "failed_jobs")
                 else:
-                    move_job_to_done(next_job, args.directory)
+                    move_job(next_job, args.directory, "done_jobs")
                     job.run_alphaplots(settings)
                     job.print_job_details()
             else:
-                move_job_to_failed(next_job, args.directory)
+                move_job(next_job, args.directory, "failed_jobs")
         else:
             log.info("Waiting for jobs. CTRL+C for interuption.")
             while not [
                 entry
                 for entry in os.scandir(input_path)
-                if entry.is_file()
-                and (entry.name.endswith(".yaml") or entry.name.endswith(".yml"))
+                if entry.is_file() and entry.name.endswith((".yaml", ".yml"))
             ]:
                 time.sleep(3)
 
